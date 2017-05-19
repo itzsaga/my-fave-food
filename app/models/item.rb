edc65ad44be41e2e8197fb2823f74855c8cb35c9
Seth <itz.saga@gmail.com>
2017-05-18 22:22:42 -0500

    added validations

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,8 @@
 class Item < ApplicationRecord
   has_many :place_items
   has_many :places, through: :place_items
+
+  validates :name, presence: true, uniqueness: true
+  validates :notes, presence: true
+  validates :rating, presence: true, numericality: { only_integer: true, less_than_or_equal_to: 5 }
 end
