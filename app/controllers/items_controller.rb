cc4f3966b4f296025683d3a0ebed28e9434a408f
Seth <itz.saga@gmail.com>
2017-05-20 14:23:47 -0500

    place_items index

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,8 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!
 
-
+  def index
+    @place = Place.find_by(id: params[:place_id])
+    @items = @place.items
+  end
 end
