9462603707df2e9df8dba49dfba63ca896e15d52
Seth <itz.saga@gmail.com>
2017-05-19 20:54:01 -0500

    added nested attributes

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,6 +2,9 @@ class Place < ApplicationRecord
   belongs_to :user
   has_many :place_items
   has_many :items, through: :place_items
+  accepts_nested_attributes_for :items
 
   validates :name, presence: true
+
+
 end
