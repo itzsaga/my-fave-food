272987d69a30b583f0324c303a86ee088ea53c1c
Seth <itz.saga@gmail.com>
2017-05-20 13:47:30 -0500

    added nested routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
 
-  resources :places
+  resources :places do
+    resources :items
+  end
 
   root 'static#home'
 end
