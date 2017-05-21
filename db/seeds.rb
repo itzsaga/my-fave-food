b29874dcbd6d259865cf77d24d3a08ca80fded74
Seth <itz.saga@gmail.com>
2017-05-21 11:35:49 -0500

    filled out seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
       name: Faker::Name.name
   )
 end
+
+@user_count = User.all.count
+
+(@user_count*2).times do
+  Place.create(
+      name: Faker::Company.name,
+      address: Faker::Address.street_address,
+      city: Faker::Address.city,
+      state: Faker::Address.state_abbr,
+      zip_code: Faker::Number.number(5),
+      user_id: rand(1..@user_count)
+  )
+end
+
+@place_count = Place.all.count
+
+(@place_count*2).times do
+  i = Item.new(
+      name: Faker::Food.ingredient,
+      rating: rand(1..5),
+      notes: Faker::Hipster.sentence
+  )
+  i.place_ids = ["#{rand(1..@user_count)}"]
+  i.save
+end
