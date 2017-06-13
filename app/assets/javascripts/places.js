4ca09bad72d9546dceef23d0ebadec85d6e185c1
Seth <itz.saga@gmail.com>
2017-06-12 22:18:00 -0500

    Finished rendering Place Items in DOM.

--- app/assets/javascripts/places.js
+++ app/assets/javascripts/places.js
@@ -4,7 +4,15 @@ $(() => {
   let path = $(location).attr('href')
   $.getJSON(path, function (data) {
     if (data.items.length > 0) {
-      console.log('more than zero')
+      let itemList = $()
+      data.items.forEach(function (item) {
+        itemList = itemList.add(`<li><strong><a href='/items/${item['id']}'>${item['name']}</a></strong>
+            <ul>
+                <li>Rating: ${item['rating']}</li>
+                <li>Notes: ${item['notes']}</li>
+            </ul>`)
+      })
+      $('#items').html(itemList)
     } else {
       $('#items').html('<div><h3>You have not added any items yet.</h3></div>')
     }
