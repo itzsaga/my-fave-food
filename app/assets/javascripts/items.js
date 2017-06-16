243c7dc623075197bc84bbbf1d2107df99ce7a56
Seth <itz.saga@gmail.com>
2017-06-15 22:37:25 -0500

    Working on Next Item button for items show page.

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -1,8 +1,28 @@
 'use strict'
 
+let itemsValues
+
 $(() => {
   let path = $(location).attr('href')
   $.getJSON(path, function (data) {
     console.log(data)
+  })
+  $.getJSON('/items.json', function (data) {
+    itemsValues = $.map(data, function (e) {
+      return e.id
+    })
+  })
+})
+
+$('.js-next').on('click', function() {
+  let nextIndex = itemsValues.indexOf(parseInt($('.js-next').attr('data-id'))) + 1
+  $.getJSON('/items/' + itemsValues[nextIndex], function(data) {
+    $('#name').html(data['name'])
+    $('#rating').html(data['rating'])
+    $('#notes').html(data['notes'])
+    if (nextIndex === itemsValues.length)
+      $('.js-next').attr('data-id', itemsValues[0])
+    else
+      $('.js-next').attr('data-id', data['id'])
   })
 })
