d0a0683b31f78bc0fa50529b0c1434ae121039bb
Seth <itz.saga@gmail.com>
2017-06-15 22:58:13 -0500

    Finished Next Item button for items show page.

--- app/assets/javascripts/items.js
+++ app/assets/javascripts/items.js
@@ -15,14 +15,18 @@ $(() => {
 })
 
 $('.js-next').on('click', function() {
-  let nextIndex = itemsValues.indexOf(parseInt($('.js-next').attr('data-id'))) + 1
+  let nextIndex
+  let dataIdIndex = itemsValues.indexOf(parseInt($('.js-next').attr('data-id')))
+  if (dataIdIndex === itemsValues.length - 1)
+    nextIndex = 0
+  else
+    nextIndex = dataIdIndex + 1
   $.getJSON('/items/' + itemsValues[nextIndex], function(data) {
-    $('#name').html(data['name'])
-    $('#rating').html(data['rating'])
-    $('#notes').html(data['notes'])
-    if (nextIndex === itemsValues.length)
-      $('.js-next').attr('data-id', itemsValues[0])
-    else
-      $('.js-next').attr('data-id', data['id'])
+    $('#name').html(`${data['name']} - 
+        <a href="/items/${data['id']}/edit">Edit</a> - 
+        <a data-confirm="Are you sure?" rel="nofollow" data-method="delete" href="/items/${data['id']}">Delete</a>`)
+    $('#rating').html(`Rating: ${data['rating']}`)
+    $('#notes').html(`Notes: ${data['notes']}`)
+    $('.js-next').attr('data-id', data['id'])
   })
 })
